1a7ffe59dec0749d11c3076ed17c87ea602e77cc
v_qiwulv <v_qiwulv@tencent.com>
2021-10-11 17:29:09 +0800

    Peizhi

--- tests/unit/sum.spec.js
+++ tests/unit/sum.spec.js
@@ -5,7 +5,7 @@ import { mount } from '@vue/test-utils'
 describe('header', () => {
   it('获取logo', () => {
     const wrapper = mount(hearder)
-    // const profile = wrapper.get('.logo')
-    console.log(wrapper, 'logo')
+    const profile = wrapper.get('.logo')
+    console.log(profile, 'logo')
   })
 })
